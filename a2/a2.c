093e2a0d7c2e950dc6d4ac393b8fd6ebf652c621
Elena Hadarau <elena.hadarau@yahoo.com>
2023-04-28 22:14:00 +0300

    with created threads

--- a2/a2.c
+++ a2/a2.c
@@ -4,12 +4,39 @@
 #include <sys/wait.h>
 #include "a2_helper.h"
 #include <stdlib.h>
-
+#include <pthread.h>
+void *threadFn6(void *unused)
+{
+    //srand(time(NULL));
+    int nrthr=(int)((long)unused);
+    info(BEGIN,6,nrthr);
+    info(END,6,nrthr);
+    return NULL;
+}
+void *threadFn3(void *unused)
+{
+    //srand(time(NULL));
+    int nrthr=(int)((long)unused);
+    info(BEGIN,3,nrthr);
+    info(END,3,nrthr);
+    return NULL;
+}
+void *threadFn5(void *unused)
+{
+    //srand(time(NULL));
+    int nrthr=(int)((long)unused);
+    info(BEGIN,5,nrthr);
+    info(END,5,nrthr);
+    return NULL;
+}
 
 int main()
 {
     init();
     int status[7]= {-1};
+    pthread_t tidp6[5]= {-1};
+    pthread_t tidp3[35]= {-1};
+    pthread_t tidp5[6]= {-1};
     //pid_t pid[7]= {-1};
     info(BEGIN, 1, 0);
     if(fork()==0)
@@ -27,10 +54,34 @@ int main()
             if(fork()==0)
             {
                 info(BEGIN,6,0);
+                for(int i=0; i<5; i++)
+                {
+                    pthread_create(&tidp6[i], NULL, threadFn6, (void*)(long)(i+1)) ;
+                }
+                for(int i=0; i<5; i++)
+                {
+                    pthread_join(tidp6[i], NULL);;
+                }
                 info(END,6,0);
+                for(int i=0; i<6; i++)
+                {
+                    pthread_create(&tidp5[i], NULL, threadFn5, (void*)(long)(i+1)) ;
+                }
+                for(int i=0; i<6; i++)
+                {
+                    pthread_join(tidp5[i], NULL);;
+                }
                 exit(0);
             }
             wait(&status[5]);
+            for(int i=0; i<35; i++)
+            {
+                pthread_create(&tidp3[i], NULL, threadFn3, (void*)(long)(i+1)) ;
+            }
+            for(int i=0; i<35; i++)
+            {
+                pthread_join(tidp3[i], NULL);;
+            }
             info(END,5,0);
             exit(0);
         }
